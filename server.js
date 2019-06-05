ff08f1def73c984df0d9044077494eb8dcb2a58e
Richi Dillarza Lugo <bartomex@hotmail.com>
2019-06-04 19:48:36 -0500

    se creo archivo en rama development

---
+++ server.js
@@ -0,0 +1,17 @@
+let http = require ("http");
+let port = 8000;
+
+function existeArchivo(solicitud, respuesta) {
+  let origen = request.url
+
+}
+
+
+const fs = require(‘fs’)
+fs.access(‘path’, fs.constants.F_OK, function (err) {
+	if (err) {
+		//El archivo no existe
+  } else {
+	//El archivo existe
+  }
+})
