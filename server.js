e387ff55db8ca9bf9a5e7cfd938d226f6d077e22
Richi Dillarza Lugo <bartomex@hotmail.com>
2019-06-04 20:26:41 -0500

    se inicio el servidor basico que está escuchando en el puerto 8000

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-let http = require ("http");
-let port = 8000;
+const http = require ("http");
+const port = 8000;
 
-function existeArchivo(solicitud, respuesta) {
-  let origen = request.url
+const server = http.createServer(manejadorDeRespuesta); //NECESITA UN CALLBACK COMO PARAMETRO
 
+function manejadorDeRespuesta(request, response) { //TAMBIEN PUEDE DECLARARSE LA FUNCION (ANOMNIMA) EN UNA VARIABLE
+  response.write("Hola desde el servidor!");
+  response.end();
 }
 
+server.listen(port, callback); //TAMBIEN PODEMOS PONER LA FUNCION EXTERNA
 
-const fs = require(‘fs’)
-fs.access(‘path’, fs.constants.F_OK, function (err) {
-	if (err) {
-		//El archivo no existe
-  } else {
-	//El archivo existe
-  }
-})
+  function callback() {
+    console.log("Iniciando servidor en puerto " + port);
+}
